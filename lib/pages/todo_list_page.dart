9abf1f01c5f227a15cc83c188776cd3e46394358
Fabio Martinelli <webmaster@zupy.com.br>
2022-07-11 22:34:13 -0300

    Text Decoration

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 
 class TodoListPage extends StatelessWidget {
-  const TodoListPage({Key? key}) : super(key: key);
+  TodoListPage({Key? key}) : super(key: key);
+
+  final TextEditingController emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.only(left: 16),
-          child: TextField(),
+          padding: const EdgeInsets.only(left: 16, right: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(labelText: 'E-mail'),
+              ),
+              ElevatedButton(
+                onPressed: login,
+                child: Text('Entrar'),
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void login() {
+    String text = emailController.text;
+    print(text);
   }
 }
