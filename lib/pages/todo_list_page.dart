2166bec86429e63b3b2c12ca34f72c8abfb56b3a
Fabio Martinelli <webmaster@zupy.com.br>
2022-07-12 21:01:07 -0300

    ListView Layout

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,36 +1,85 @@
 import 'package:flutter/material.dart';
 
 class TodoListPage extends StatelessWidget {
-  TodoListPage({Key? key}) : super(key: key);
-
-  final TextEditingController emailController = TextEditingController();
+  const TodoListPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.only(left: 16, right: 16),
+          padding: const EdgeInsets.symmetric(horizontal: 16),
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              TextField(
-                controller: emailController,
-                decoration: InputDecoration(labelText: 'E-mail'),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicione uma Tarefa',
+                        hintText: 'Ex. Estudar Flutter',
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.purple,
+                      padding: EdgeInsets.all(14),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                  ),
+                ],
               ),
-              ElevatedButton(
-                onPressed: login,
-                child: Text('Entrar'),
+              SizedBox(height: 16),
+              //listview
+              ListView(
+                shrinkWrap: true,
+                children: [
+                  ListTile(
+                    title: Text('Tarefa 1'),
+                    subtitle: Text('12/07/2022'),
+                    leading: Icon(
+                      Icons.save,
+                      size: 30,
+                    ),
+                    onTap: () {
+                      print('Tarefa 1');
+                    },
+                  )
+                ],
+              ),
+              //linha de baixo
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Você possui 0 Tarefas Pendentes',
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.purple,
+                      padding: EdgeInsets.all(14),
+                    ),
+                    child: Text('Limpar Tudo'),
+                  )
+                ],
               )
             ],
           ),
         ),
       ),
     );
-  }
-
-  void login() {
-    String text = emailController.text;
-    print(text);
   }
 }
