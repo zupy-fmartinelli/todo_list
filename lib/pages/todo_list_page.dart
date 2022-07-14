3eb980f086fc783f85de827a406799f0df02f829
Fabio Martinelli <webmaster@zupy.com.br>
2022-07-13 21:30:26 -0300

    Date time da lista formatado

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/models/todo.dart';
 
 import '../widgets/todo_list_item.dart';
 
@@ -12,7 +13,7 @@ class TodoListPage extends StatefulWidget {
 class _TodoListPageState extends State<TodoListPage> {
   final TextEditingController todoController = TextEditingController();
 
-  List<String> todos = [];
+  List<Todo> todos = [];
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +43,11 @@ class _TodoListPageState extends State<TodoListPage> {
                       onPressed: () {
                         String text = todoController.text;
                         setState(() {
-                          todos.add(text);
+                          Todo newTodo = Todo(
+                            title: text,
+                            dateTime: DateTime.now(),
+                          );
+                          todos.add(newTodo);
                         });
                         todoController.clear();
                       },
@@ -63,9 +68,9 @@ class _TodoListPageState extends State<TodoListPage> {
                   child: ListView(
                     shrinkWrap: true,
                     children: [
-                      for (String todo in todos)
+                      for (Todo todo in todos)
                         TodoListItem(
-                          title: todo,
+                          todo: todo,
                         ),
                     ],
                   ),
--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:intl/intl.dart';
+import 'package:todo_list/models/todo.dart';
 
 class TodoListItem extends StatelessWidget {
-  const TodoListItem({Key? key, required this.title}) : super(key: key);
+  const TodoListItem({Key? key, required this.todo}) : super(key: key);
 
-  final String title;
+  final Todo todo;
 
   @override
   Widget build(BuildContext context) {
@@ -21,13 +23,13 @@ class TodoListItem extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            '20/11/2022',
+            DateFormat('dd/MM/yy - HH:mm').format(todo.dateTime),
             style: TextStyle(
               fontSize: 12,
             ),
           ),
           Text(
-            title,
+            todo.title,
             style: TextStyle(
               fontSize: 16,
               fontWeight: FontWeight.w600,
